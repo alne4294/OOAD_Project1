d9a22fac2dc6e359627ce0bbe74728d9621abe48
Alexia Newgord <alexia.newgord@colorado.edu>
2014-04-22 18:54:05 -0600

    Trying to get detailView to work...

--- htdocs/protected/modules/hospital/controllers/DefaultController.php
+++ htdocs/protected/modules/hospital/controllers/DefaultController.php
@@ -4,9 +4,72 @@ class DefaultController extends CController
 {
     
     	public $layout='/layouts/column1';
-	
-        public function actionIndex()
+
+        public function actionIndex($id = null)
 	{
-		$this->render('index');
+            
+            $model=$this->loadModel($id);
+            //$dataProvider=new Patient();
+            
+           if(Yii::app()->request->isAjaxRequest){
+               echo "I'm here!!!"; 
+               $id = $_GET[0];
+                $model=$this->loadModel($id);
+                //$dataProvider->criteria = array('condition'=>'id='.$id);
+                echo CJSON::encode($model);
+            }
+            
+            $dataProvider=new CActiveDataProvider(Patient::model(), array(
+                    'keyAttribute'=>'id',//needed for gridview to know selection?
+                    'pagination'=>array(
+                            'pageSize'=>20,
+                    ),
+                    'sort'=>array(
+                            'defaultOrder'=>array('timestamp'=>true),
+                    ),
+            ));
+            
+            //$dataProvider->unsetAttributes();
+            
+            if(isset($_GET['Patient']))
+                $dataProvider->attributes=$_GET['Patient'];
+	    
+            $this->render('index', array(
+                'model'=>$model, 'dataProvider'=>$dataProvider));
+	}
+        
+         /**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return Patient the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($id)
+	{
+		if($id==null)
+                    $id=2;
+                $model=Patient::model()->findByPk($id);
+		if($model===null)
+                    //$model=$dataProvider;
+                    throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+        
+        public function actionObtainPatientInfo($id){
+		//$response = Patient::model()->findAllByAttributes(array('id'=>$id));
+                $model=$this->loadModel($id);
+		header("Content-type: application/json");
+		//echo CJSON::encode($model);
+
+                $newmodel = CJSON::encode($model);
+                $array = json_decode($newmodel, true);
+                var_dump($array);
+                /*
+                //$this->render('index',array(
+		//	'model'=>$model,
+		//));
+                
+                return CJSON::encode($model);*/
 	}
 }
--- htdocs/protected/modules/hospital/views/default/index.php
+++ htdocs/protected/modules/hospital/views/default/index.php
@@ -1,26 +1,82 @@
-<?php
-/* @var $this DefaultController */
-
-?>
-
 
 <div id="frameLeft">
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<?php
-    $this->widget('zii.widgets.grid.CGridView',array(
-        'dataProvider'=>$model->search(),
-        
-    ));
-?>
+    
+    
+     <script type="text/javascript">
+        function selectRow(target_id) {
+            alert($.fn.yiiGridView.getSelection(target_id));
+            
+            var id = $.fn.yiiGridView.getSelection(target_id);
+            //$.fn.yiiGridView.update('patient',{ data: target_id });
+            
+            var action='index.php/hospital/default/id/'+id;
+            /*
+            $.getJSON(action, function(data) {
+                alert(data.id);
+                alert(data.last);
+                $('#frameRight').html('<p> Name: ' + data.id + '</p>');
+                $('#frameRight').append('<p>Last : ' + data.last+ '</p>');
+
+            });*/
+        }
+    </script>  
+
+    
+    
+    <?php
+        $this->widget('zii.widgets.grid.CGridView',array(
+            'id'=>'myGrid',
+            'selectableRows'=>1,            
+            'dataProvider'=>$dataProvider,
+            //'filter'=>$model,
+            'htmlOptions'=>array('style'=>'cursor: pointer;'),
+            'columns'=>array(
+                array('name'=>'id', value=>'$data->id','htmlOptions'=>array('width'=>'40px')),
+                array('name'=>'timestamp','value'=>'$data->timestamp','htmlOptions'=>array('width'=>'150px')),
+                array('name'=>'last','value'=>'$data->last'),
+                array('name'=>'first','value'=>'$data->first',),
+                array('name'=>'dob','value'=>'$data->dob',),
+                /*array(
+                    'class'=>'CButtonColumn',
+                    'template'=>'{view}{received}{treated}',//View, Mark received, Mark treated
+                    'view'=>array(
+                             'url'=>'Yii::app()->controller->createUrl("view",array("id"=>$data->primaryKey))',
+                             //'click'=>'function(){$("#cru-frame").attr("src",$(this).attr("href")); $("#cru-dialog").dialog("open");  return false;}',
+                             ),
+                   'delete'=>array(
+                             'url'=>'$this->grid->controller->createUrl("/History/delete", array("id"=>$data->primaryKey,"asDialog"=>1,"gridId"=>$this->grid->id))',
+                             ),
+                 ),*/
+            ),
+            'selectionChanged'=>selectRow,//'CHtml::link(CHtml::encode($data->id), array(\'index\', \'id\'=>$data->id))',//
+        ));
+    ?>
 </div>
 
 <div id="frameRight">
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>  
+
+
+    <?php $this->widget('zii.widgets.CDetailView', array(
+            'data'=>$model,
+            'attributes'=>array(
+                    'id',
+                    'name',
+                    'last',
+                    'first',
+                    'dob',
+                    'phone_primary',
+                    'phone_secondary',
+                    'phone_emergency',
+                    'address',
+                    'city',
+                    'state',
+                    'zip',
+                    'country',
+                    'timestamp',
+                array(
+                    'name'=>'last',
+                    'value'=>$data->last,
+                ),
+            ),
+    )); ?>
 </div>
