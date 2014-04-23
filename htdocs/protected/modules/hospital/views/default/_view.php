c20f45d6e08df95bd19185fe556eee38fd12c543
Alexia Newgord <alexia.newgord@colorado.edu>
2014-04-22 23:05:34 -0600

    fixed CDetailView!

--- htdocs/protected/modules/hospital/views/default/_view.php
+++ htdocs/protected/modules/hospital/views/default/_view.php
@@ -5,63 +5,31 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('last')); ?>:</b>
-	<?php echo CHtml::encode($data->last); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('first')); ?>:</b>
-	<?php echo CHtml::encode($data->first); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('dob')); ?>:</b>
-	<?php echo CHtml::encode($data->dob); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('phone_primary')); ?>:</b>
-	<?php echo CHtml::encode($data->phone_primary); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('phone_secondary')); ?>:</b>
-	<?php echo CHtml::encode($data->phone_secondary); ?>
-	<br />
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('phone_emergency')); ?>:</b>
-	<?php echo CHtml::encode($data->phone_emergency); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('address')); ?>:</b>
-	<?php echo CHtml::encode($data->address); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('city')); ?>:</b>
-	<?php echo CHtml::encode($data->city); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('state')); ?>:</b>
-	<?php echo CHtml::encode($data->state); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('zip')); ?>:</b>
-	<?php echo CHtml::encode($data->zip); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('country')); ?>:</b>
-	<?php echo CHtml::encode($data->country); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('timestamp')); ?>:</b>
-	<?php echo CHtml::encode($data->timestamp); ?>
-	<br />
-
-	*/ ?>
+    <?php $this->widget('zii.widgets.CDetailView', array(
+            'data'=>$model,
+            'attributes'=>array(
+                    'id',
+                    'name',
+                    'last',
+                    'first',
+                    'dob',
+                    'phone_primary',
+                    'phone_secondary',
+                    'phone_emergency',
+                    'address',
+                    'city',
+                    'state',
+                    'zip',
+                    'country',
+                    'timestamp',
+                array(
+                    'name'=>'last',
+                    'value'=>$data->last,
+                ),
+            ),
+    )); ?>
+    
+    
+    
 
 </div>
